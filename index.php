ec906962b1f1239f60c383342bb3d7163bfa6788
Shaji <code@shaji.in>
2023-03-27 12:53:23 +0530

    index

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
   </head>
   <body>
     <div class="row">
-		
+      <div class="col-md-4 card shadow">
+        <img src="/logo.png" class="img-fluid">
+        <h3>Where do you want to go today?</h3>
+      </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
