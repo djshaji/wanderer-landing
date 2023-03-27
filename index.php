63b13373522f3f10a0a8aa9122e3191530e78315
Shaji <code@shaji.in>
2023-03-27 12:56:25 +0530

    index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-    <div class="justify-content-center row">
+    <div class="d-flex justify-content-center row">
       <div class="col-md-4 card shadow justify-content-center">
         <img src="/logo.png" class="img-fluid" width="100">
         <h5>Where do you want to go today?</h5>
