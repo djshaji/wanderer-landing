70aba1f5afcdd9a60f4b72cf66c87c34f38492ae
Shaji <code@shaji.in>
2023-03-27 13:03:48 +0530

    index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <body>
   <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
     <img src="/logo.png" width="200"></img>
-    <p class="alert alert-primary shadow m-3">Where do you want to go today?</p>
+    <!-- <p class="alert alert-primary shadow m-3">Where do you want to go today?</p> -->
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
